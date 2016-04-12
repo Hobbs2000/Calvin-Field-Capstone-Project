81159f75a771e0757a9033b1d5fcfc3a8656fae2
Hobbs2000 <hobbs2018@gmail.com>
2016-04-12 18:50:27 -0500

    Created a player handler class

---
+++ PlayerHandler.java
@@ -0,0 +1,10 @@
+
+/**
+ * This will handle the players movements and collision detection
+ * Will be on a separate thread
+ * Created by Calvin on 4/12/2016.
+ */
+public class PlayerHandler
+{
+    
+}
