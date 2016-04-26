b16bd6fc0b14391198250bb076e97fb276f3662b
Hobbs2000 <CJField@student.naperville203.org>
2016-04-26 14:14:32 -0500

    Started making a better main class and started on a separate render class

--- Main.java
+++ Main.java
@@ -1,12 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+
 /**
  * Created by Calvin on 4/7/2016.
+ * 
  */
-public class Main implements Runnable
+public class Main extends Canvas implements Runnable
 {
-    private boolean running;
+    public JFrame frame;
+    private boolean running = false;
+    private ArrayList<Entity> entities = new ArrayList<Entity>();
+    private Level level = new Level();
+    private final int WAIT = 10; //All thread sleeps need to be the same time to prevent visible stuttering
 
+    /**
+     * 
+     */
+    public Main()
+    {
+        Dimension size = new Dimension(1216, 1024);
+        //setPreferredSize is in the canvas class
+        setPreferredSize(size);
+        frame = new JFrame();
+    }
+    
+    /**
+     * 
+     */
     public void run()
     {
-        while (running);
+        
+        while (running)
+        {
+            
+        }
+    }
+    
+    /**
+     * 
+     */
+    public synchronized void start()
+    {
+        running = true;
     }
 }
---
+++ Renderer.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class Renderer
+{
+    Canvas thisCanvas;
+    
+}
